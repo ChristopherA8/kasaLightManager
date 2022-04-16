f672b9f9b1a85e478feac35c849fa603bc86f986
Chr1sDev <christopheraanderson2@gmail.com>
2022-04-16 12:52:39 -0500

    add the rest of the lights

--- src/LightManger.js
+++ src/LightManger.js
@@ -8,8 +8,18 @@ class LightManager {
       host: "192.168.50.34",
       port: "9999",
     });
+    this.bulb2 = await this.client.getDevice({
+      host: "192.168.50.167",
+      port: "9999",
+    });
+    this.bulb3 = await this.client.getDevice({
+      host: "192.168.50.237",
+      port: "9999",
+    });
 
     await this.bulb1.setPowerState(!(await this.bulb1.getPowerState()));
+    await this.bulb2.setPowerState(!(await this.bulb2.getPowerState()));
+    await this.bulb3.setPowerState(!(await this.bulb3.getPowerState()));
   }
 }
 
